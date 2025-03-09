fb7b73d0ba2c9e1f6d2fe1c36234ff6d20fc8017
jn-devops <devops@joy-nostalg.com>
2025-03-09 22:08:27 +0800

    update metadata size

---
+++ database/migrations/2025_03_09_220545_update_metadata_size_on_redeemers.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use FrittenKeeZ\Vouchers\Config;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table(Config::table('redeemers'), function (Blueprint $table) {
+            $table->longText('metadata')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table(Config::table('redeemers'), function (Blueprint $table) {
+            $table->text('metadata')->nullable()->change();
+        });
+    }
+};
